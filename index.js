6c2fa9c96560ccf1def4681090e4ad852672acd1
modern-fork-2657 <kalkidan.solomon2@gmail.com>
2020-04-06 22:37:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ function prependKitten(name){
 };
 
 function removeLastKitten(){
-  kittens.splice(0, kittens.length-1)
-  return kittens
+  let newArray=kittens.concat(kittens.splice(0, kittens.length-1))
+  return newArray
 }
