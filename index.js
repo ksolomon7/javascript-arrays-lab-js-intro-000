842338749a855844df7cf470b657bc445147beba
modern-fork-2657 <kalkidan.solomon2@gmail.com>
2020-04-06 22:18:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var kittens = ['Milo','Otis','Garfield'];
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-  return appendKittens(name);
-} 
+  return kitten.unshift(name);
+}
