ac86ecc1c0a4ec4904df39b978fb81eef2a5768d
modern-fork-2657 <kalkidan.solomon2@gmail.com>
2020-04-06 22:21:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ function destructivelyPrependKitten(name){
 function destructivelyRemoveLastKitten(name){
   return kittens.pop(name);
 }
+
+function destructivelyRemoveFirstKitten(array){
+  return kittens.shift();
+}
